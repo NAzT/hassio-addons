a27c2be7d5ca9a7f51ce0b8340f0e0fe94e8ed4c
Nat Weerawan <nat.wrw@gmail.com>
2021-04-02 01:36:06 +0700

    Update adapter.go

--- rtsp2webrtc/app/adapter.go
+++ rtsp2webrtc/app/adapter.go
@@ -56,7 +56,7 @@ func NewPeerConnection(configuration webrtc.Configuration) (*webrtc.PeerConnecti
 
 	s := webrtc.SettingEngine{}
 	s.SetEphemeralUDPPortRange(10550, 50009)
-	log.Println("Set UDP ports to 50000..50009")
+	log.Println("Set UDP ports to 10550..50009")
 	api := webrtc.NewAPI(webrtc.WithMediaEngine(m), webrtc.WithInterceptorRegistry(i), webrtc.WithSettingEngine(s))
 	return api.NewPeerConnection(configuration)
 }
